22d323a1c0f9916fda4393a768f4fecdae35f63a
dongNguyen <dongvn666@gmail.com>
2019-04-08 08:16:31 +0700

    Updates

---
+++ precache-manifest.ba75cd9f8b9410cdd20349439361895b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f95493ce3f037a18dc0d9f4effb24e01",
+    "url": "/calculator/static/media/plus-solid.f95493ce.svg"
+  },
+  {
+    "revision": "e2b90ea6751a3ea7ac46",
+    "url": "/calculator/static/css/main.e002faf3.chunk.css"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "e3bc29623cec1e23b76e",
+    "url": "/calculator/static/js/2.33cd2c08.chunk.js"
+  },
+  {
+    "revision": "91427a13b77cadcca17cba1262408830",
+    "url": "/calculator/static/media/percentage-solid.91427a13.svg"
+  },
+  {
+    "revision": "e2b90ea6751a3ea7ac46",
+    "url": "/calculator/static/js/main.39b0e542.chunk.js"
+  },
+  {
+    "revision": "af44842222f60ee9388dbc3866e83aaa",
+    "url": "/calculator/static/media/minus-solid.af448422.svg"
+  },
+  {
+    "revision": "cffb8b796d80d725f61730cdf5316d25",
+    "url": "/calculator/static/media/divide-solid.cffb8b79.svg"
+  },
+  {
+    "revision": "9900062bd7b52651c149e0d5fb19058f",
+    "url": "/calculator/static/media/equals-solid.9900062b.svg"
+  },
+  {
+    "revision": "7828a596910f9318123e63dfa3f9571f",
+    "url": "/calculator/static/media/times-solid.7828a596.svg"
+  },
+  {
+    "revision": "c738f2492157bc2bc8bd1de3f8e0bd17",
+    "url": "/calculator/index.html"
+  }
+];
